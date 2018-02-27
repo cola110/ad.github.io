45463ea08ae92bb116ed7c7a408f26535405b135
zhangzt <zhangzt@ctrip.com>
2018-02-27 18:33:57 +0800

    update

---
+++ admin/scripts/com/component.js
@@ -0,0 +1,87 @@
+Vue.component("silder", {
+    template: '   <div class="left-sidebar">\n' +
+    '        <!-- 用户信息 -->\n' +
+    '        <div class="tpl-sidebar-user-panel">\n' +
+    '            <div class="tpl-user-panel-slide-toggleable">\n' +
+    '                <div class="tpl-user-panel-profile-picture">\n' +
+    '                    <img src="assets/img/user04.png" alt="">\n' +
+    '                </div>\n' +
+    '                <span class="user-panel-logged-in-text">\n' +
+    '              <i class="am-icon-circle-o am-text-success tpl-user-panel-status-icon"></i>\n' +
+    '              禁言小张\n' +
+    '          </span>\n' +
+    '                <a href="javascript:;" class="tpl-user-panel-action-link"> <span class="am-icon-pencil"></span> 账号设置</a>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '\n' +
+    '        <!-- 菜单 -->\n' +
+    '        <ul class="sidebar-nav">\n' +
+    '            <li class="sidebar-nav-heading">Components <span class="sidebar-nav-heading-info"> 附加组件</span></li>\n' +
+    '            <li class="sidebar-nav-link">\n' +
+    '                <a href="index.html" class="active">\n' +
+    '                    <i class="am-icon-home sidebar-nav-link-logo"></i> 首页\n' +
+    '                </a>\n' +
+    '            </li>\n' +
+    '            <li class="sidebar-nav-link">\n' +
+    '                <a href="db/index.html">\n' +
+    '                    <i class="am-icon-table sidebar-nav-link-logo"></i> 数据库管理\n' +
+    '                </a>\n' +
+    '            </li>\n' +
+    '            <li class="sidebar-nav-link">\n' +
+    '                <a href="calendar.html">\n' +
+    '                    <i class="am-icon-calendar sidebar-nav-link-logo"></i> 日历\n' +
+    '                </a>\n' +
+    '            </li>\n' +
+    '            <li class="sidebar-nav-link">\n' +
+    '                <a href="form.html">\n' +
+    '                    <i class="am-icon-wpforms sidebar-nav-link-logo"></i> 表单\n' +
+    '\n' +
+    '                </a>\n' +
+    '            </li>\n' +
+    '            <li class="sidebar-nav-link">\n' +
+    '                <a href="chart.html">\n' +
+    '                    <i class="am-icon-bar-chart sidebar-nav-link-logo"></i> 图表\n' +
+    '\n' +
+    '                </a>\n' +
+    '            </li>\n' +
+    '\n' +
+    '            <li class="sidebar-nav-heading">Page<span class="sidebar-nav-heading-info"> 常用页面</span></li>\n' +
+    '            <li class="sidebar-nav-link">\n' +
+    '                <a href="javascript:;" class="sidebar-nav-sub-title">\n' +
+    '                    <i class="am-icon-table sidebar-nav-link-logo"></i> 数据列表\n' +
+    '                    <span class="am-icon-chevron-down am-fr am-margin-right-sm sidebar-nav-sub-ico"></span>\n' +
+    '                </a>\n' +
+    '                <ul class="sidebar-nav sidebar-nav-sub">\n' +
+    '                    <li class="sidebar-nav-link">\n' +
+    '                        <a href="table-list.html">\n' +
+    '                            <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 文字列表\n' +
+    '                        </a>\n' +
+    '                    </li>\n' +
+    '\n' +
+    '                    <li class="sidebar-nav-link">\n' +
+    '                        <a href="table-list-img.html">\n' +
+    '                            <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 图文列表\n' +
+    '                        </a>\n' +
+    '                    </li>\n' +
+    '                </ul>\n' +
+    '            </li>\n' +
+    '            <li class="sidebar-nav-link">\n' +
+    '                <a href="sign-up.html">\n' +
+    '                    <i class="am-icon-clone sidebar-nav-link-logo"></i> 注册\n' +
+    '                    <span class="am-badge am-badge-secondary sidebar-nav-link-logo-ico am-round am-fr am-margin-right-sm">6</span>\n' +
+    '                </a>\n' +
+    '            </li>\n' +
+    '            <li class="sidebar-nav-link">\n' +
+    '                <a href="login.html">\n' +
+    '                    <i class="am-icon-key sidebar-nav-link-logo"></i> 登录\n' +
+    '                </a>\n' +
+    '            </li>\n' +
+    '            <li class="sidebar-nav-link">\n' +
+    '                <a href="404.html">\n' +
+    '                    <i class="am-icon-tv sidebar-nav-link-logo"></i> 404错误\n' +
+    '                </a>\n' +
+    '            </li>\n' +
+    '\n' +
+    '        </ul>\n' +
+    '    </div>\n'
+});
